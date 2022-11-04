1af411cd39ad591d0fbd2eea05fa73648b9fd0b7
rgarro <rgarro@gmail.com>
2022-11-04 14:51:45 -0500

    Initial Commit

---
+++ Locatapp/LocatappApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocatappApp.swift
+//  Locatapp
+//
+//  Created by Rolando Garro on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct LocatappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
