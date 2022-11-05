a4d0ebe4d3d30eba985f0094b2c7983317b66c78
rgarro <rgarro@gmail.com>
2022-11-05 14:47:41 -0500

    my Slingshot Turbine is on the UPS truck

--- Locatapp/ContentView.swift
+++ Locatapp/ContentView.swift
@@ -1,6 +1,12 @@
 //
 //  ContentView.swift
 //  Locatapp
+//Swift Creek is on my way when depositing
+// pay checks at the Bank, theres always
+// a line of turtles taking the sun there
+// few yards east from swift creek theres a starbucks
+//they have osprey helicopter paintings at starbucks
+//my new Slingshot Turbine is on the UPS truck
 //
 //  Created by Rolando Garro on 11/4/22.
 //
@@ -9,7 +15,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Que onda wey!!!")
+        Text("Que Onda Wey!!!")
             .padding()
     }
 }
