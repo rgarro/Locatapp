25c8df23f515e330b7473c649e8e0e588772a860
rgarro <rgarro@gmail.com>
2022-11-05 14:54:53 -0500

    pete cabrinha doesn't want to sponsor my trip to maui ..

--- Locatapp/ContentView.swift
+++ Locatapp/ContentView.swift
@@ -1,12 +1,22 @@
 //
 //  ContentView.swift
 //  Locatapp
+//---------------+---------------
+//          ___ /^^[___              _
+//         /|^+----+   |#___________//
+//       ( -+ |____|   _______-----+/
+//        ==_________--'            \
+//          ~_|___|__
+//
 //Swift Creek is on my way when depositing
 // pay checks at the Bank, theres always
 // a line of turtles taking the sun there
 // few yards east from swift creek theres a starbucks
 //they have osprey helicopter paintings at starbucks
 //my new Slingshot Turbine is on the UPS truck
+//This app will Geo Count KiteSurf Sessions
+// there should be iWatch beeper feeding the open session
+// Remeber eat a cake at starbucks when finished ...
 //
 //  Created by Rolando Garro on 11/4/22.
 //
