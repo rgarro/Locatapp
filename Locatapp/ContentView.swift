4c9bd6edd5db637c9f47828cd07920f3bfa62631
rgarro <rgarro@gmail.com>
2022-11-05 14:20:18 -0500

    que onda rey again ..

--- Locatapp/ContentView.swift
+++ Locatapp/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Que onda wey!")
+        Text("Que onda wey!!!")
             .padding()
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Locatapp
+The beeping plate of a cakePhp Backend, tracking kitesurf session from my iWatch , counting times, positions, barometric presure, temperature ..
