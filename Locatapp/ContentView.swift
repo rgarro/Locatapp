ab48cbb093da27c320d478eea7e339229ba82ae2
rgarro <rgarro@gmail.com>
2022-11-06 12:35:24 -0600

    float int the sum er sky 99 red ballons goBy

--- Locatapp/ContentView.swift
+++ Locatapp/ContentView.swift
@@ -25,8 +25,11 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Que Onda Wey!!!")
-            .padding()
+        Text("Que Onda Wey!")
+            .padding();
+        Button(/*@START_MENU_TOKEN@*/"Button"/*@END_MENU_TOKEN@*/) {
+            /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+        }
     }
 }
 
